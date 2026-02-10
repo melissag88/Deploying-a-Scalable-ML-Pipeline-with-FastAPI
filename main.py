0717d7de79888892e348f4b5d8fd5e5abb51e322
melissag88 <melissag88@outlook.com>
2026-02-10 23:30:57 +0000

    Completed ML pipeline, API, local tests, GET and PUSH messages

--- main.py
+++ main.py
@@ -26,21 +26,21 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
-encoder = load_model(path)
+encoder_path = os.path.join('model', 'encoder.pkl') # TODO: enter the path for the saved encoder 
+encoder = load_model(encoder_path)
 
-path = None # TODO: enter the path for the saved model 
-model = load_model(path)
+model_path = os.path.join('model', 'model.pkl') # TODO: enter the path for the saved model 
+model = load_model(model_path)
 
 # TODO: create a RESTful API using FastAPI
-app = None # your code here
+app = FastAPI() # your code here
 
 # TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
     # your code here
-    pass
+    return {'message': 'Hello from the API!!'}
 
 
 # TODO: create a POST on a different path that does model inference
@@ -51,8 +51,8 @@ async def post_inference(data: Data):
     # DO NOT MODIFY: clean up the dict to turn it into a Pandas DataFrame.
     # The data has names with hyphens and Python does not allow those as variable names.
     # Here it uses the functionality of FastAPI/Pydantic/etc to deal with this.
-    data = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
-    data = pd.DataFrame.from_dict(data)
+    data_df = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
+    data_df = pd.DataFrame.from_dict(data_df)
 
     cat_features = [
         "workclass",
@@ -69,6 +69,12 @@ async def post_inference(data: Data):
         # use data as data input
         # use training = False
         # do not need to pass lb as input
+        data_df,
+        categorical_features=cat_features,
+        label=None, 
+        training=False,
+        encoder=encoder,
+        lb=None
     )
-    _inference = None # your code here to predict the result using data_processed
-    return {"result": apply_label(_inference)}
+    preds = inference(model, data_processed) # your code here to predict the result using data_processed
+    return {"result": apply_label(preds)}
